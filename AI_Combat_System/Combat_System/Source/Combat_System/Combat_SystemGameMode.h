7b43a1cfea5d25d8495a2f1bc68404a7b7b24636
Damian Andrysiak <andrydamian@gmail.com>
2020-09-10 23:30:00 +0200

    added a project

---
+++ AI_Combat_System/Combat_System/Source/Combat_System/Combat_SystemGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Combat_SystemGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACombat_SystemGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACombat_SystemGameMode();
+};
+
+
+
