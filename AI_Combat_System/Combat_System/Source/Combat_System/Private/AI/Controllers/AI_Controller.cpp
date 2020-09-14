17e894d0d457d519f3da6488dfbee1fa72a82e96
Damian Andrysiak <andrydamian@gmail.com>
2020-09-14 11:31:05 +0200

    splited one BT into two separate ones

--- AI_Combat_System/Combat_System/Source/Combat_System/Private/AI/Controllers/AI_Controller.cpp
+++ AI_Combat_System/Combat_System/Source/Combat_System/Private/AI/Controllers/AI_Controller.cpp
@@ -2,6 +2,10 @@
 
 #include "../../../Public/AI/Controllers/AI_Controller.h"
 
+void AAI_Controller::Set_Behavior_Type(eNPC_Character_Type eType)
+{
+
+}
 
 void AAI_Controller::Cos()
 {
--- AI_Combat_System/Combat_System/Source/Combat_System/Public/AI/Controllers/AI_Controller.h
+++ AI_Combat_System/Combat_System/Source/Combat_System/Public/AI/Controllers/AI_Controller.h
@@ -11,8 +11,20 @@ class COMBAT_SYSTEM_API AAI_Controller : public AAIController
 {
 	GENERATED_BODY()
 	
+private:
+	enum class eNPC_Character_Type : std::int8_t
+	{
+		Friendly,
+		Lossing_Patience,
+		Aggressive
+	};
+
 public:
+	//UFUNCTION(BlueprintCallable, Category = "Behavior_Type")
+	void Set_Behavior_Type(eNPC_Character_Type eType);
 
 	UFUNCTION(BlueprintCallable, Category = "Cos")
 	void Cos();
+
+
 };
