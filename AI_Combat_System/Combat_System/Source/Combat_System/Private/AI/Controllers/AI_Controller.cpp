b1be4d20c99869216195a64acbbf03e0c08255e6
Damian Andrysiak <andrydamian@gmail.com>
2020-09-16 11:43:56 +0200

    clean up AI controller

--- AI_Combat_System/Combat_System/Source/Combat_System/Private/AI/Controllers/AI_Controller.cpp
+++ AI_Combat_System/Combat_System/Source/Combat_System/Private/AI/Controllers/AI_Controller.cpp
@@ -1,13 +1,3 @@
 
 
 #include "../../../Public/AI/Controllers/AI_Controller.h"
-
-//void AAI_Controller::Set_Behavior_Type(NPC_Character_Type eType)
-//{
-//
-//}
-
-void AAI_Controller::Cos()
-{
-
-}
--- AI_Combat_System/Combat_System/Source/Combat_System/Public/AI/Controllers/AI_Controller.h
+++ AI_Combat_System/Combat_System/Source/Combat_System/Public/AI/Controllers/AI_Controller.h
@@ -6,9 +6,6 @@
 #include "AI_Controller.generated.h"
 
 
-
-
-
 UCLASS()
 class COMBAT_SYSTEM_API AAI_Controller : public AAIController
 {
@@ -21,14 +18,5 @@ private:
 		Lossing_Patience,
 		Aggressive
 	};
-	//UPROPERTY(EditAnywhere, Category = "Behavior_Type", meta = (AllowPrivateAccess = "true"))
-	NPC_Character_Type type;
-public:
-	//UFUNCTION(BlueprintCallable, Category = "Behavior_Type")
-	//void Set_Behavior_Type(enum NPC_Character_Type eType);
-
-	UFUNCTION(BlueprintCallable, Category = "Cos")
-		void Cos();
-
 
 };
